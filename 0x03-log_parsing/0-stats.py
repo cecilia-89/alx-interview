0afc0c1b7f5441b299c42bca5b58d74dd7df643f
cecilia-89 <catabong89@gmail.com>
2023-01-21 20:39:34 +0100

    Log Parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Log Parsing"""
+import sys
+import re
+
+fileSize = 0
+statusCodes = {}
+for count, line in enumerate(sys.stdin, start = 1):
+    try:
+        words = line.split()
+        fileSize += int(words[-1])
+        value = statusCodes.setdefault(words[-2], 0)
+        statusCodes[words[-2]] = value + 1
+
+        if (count) % 10 == 0:
+            print("File size: {}".format(fileSize))
+            [print(f'{k}: {v}') for k, v in sorted(statusCodes.items())]
+
+    except KeyboardInterrupt:
+        print("File size: {}".format(fileSize))
+        [print(f'{k}: {v}') for k, v in sorted(statusCodes.items())]
+
