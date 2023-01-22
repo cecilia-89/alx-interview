55a3d985886cd7b281a48cd22b93b2f110af1819
cecilia-89 <catabong89@gmail.com>
2023-01-22 08:38:10 +0100

    Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,8 +4,7 @@ import sys
 
 lines = sys.stdin.readlines()
 fileSize = 0
-statusCodes = {'200': 0, '301': 0, '400': 0, '401': 0,
-               '403': 0, '404': 0, '405': 0, '500': 0}
+statusCodes = {}
 
 if len(lines) == 0:
     print('File size: {}'.format(0))
@@ -14,17 +13,17 @@ try:
         words = line.split()
         fileSize += int(words[-1])
 
-        value = statusCodes.get(words[-2])
-        if words[-2].isdigit() and value is not None:
+        if words[-2].isdigit():
+            value = statusCodes.setdefault(words[-2], 0)
             statusCodes[words[-2]] = value + 1
 
         if count == len(lines) or (count % 10) == 0:
             print("File size: {}".format(fileSize))
-            [print('{}: {}'.format(k, v)) for k, v in statusCodes.items()
+            [print('{}: {}'.format(k, v)) for k, v in sorted(statusCodes.items())
              if v != 0]
 
 except KeyboardInterrupt:
     print("File size: {}".format(fileSize))
     [print('{}: {}'.format(k, v))
-     for k, v in statusCodes.items()
+     for k, v in sorted(statusCodes.items())
      if v != 0]
