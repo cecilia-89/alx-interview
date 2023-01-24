c9967ebfb97f282b5e72bbbc483b06d46046159e
cecilia-89 <catabong89@gmail.com>
2023-01-24 11:09:09 +0100

    pycodestyle compliant

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,19 @@
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8"""
+    if len(data) == 0:
+        return False
     for item in data:
-        if item > 255:
+        if item > 255 or item < 0:
             return False
     return True
+
+
+data = []
+print(validUTF8(data))
+
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
+
+data = [229, 65, 127, 256]
+print(validUTF8(data))
