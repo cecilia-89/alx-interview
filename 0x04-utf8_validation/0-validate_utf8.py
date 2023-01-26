8066c924cee7b5a9a3ed2be649a622e4a51680d4
cecilia-89 <catabong89@gmail.com>
2023-01-26 13:30:35 +0100

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,13 +4,7 @@
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8"""
-    if len(data) == 0:
-        return False
     for item in data:
-        if type(item) == str:
-            decimal = ord(item)
-        else:
-            decimal = item
-        if decimal > 255:
+        if item > 255:
             return False
     return True
