a1b063006562f96cc570c3ff3fa69fc6283ace94
cecilia-89 <catabong89@gmail.com>
2023-03-03 09:08:19 +0100

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module: Island Perimeter"""
+
+
+def island_perimeter(grid):
+    perimeter = 0
+    for count, array in enumerate(grid):
+        for i, item in enumerate(array):
+            if item == 1:
+                if array[i+1] == 0 and array[i-1] == 0:
+                    perimeter += 2
+
+                if array[i+1] == 1 and array[i-1] == 0 or \
+                        array[i+1] == 0 and array[i-1] == 1:
+                    perimeter += 1
+
+                if grid[count - 1][i] == 0 and \
+                        grid[count + 1][i] == 0:
+                    perimeter += 2
+
+                if grid[count - 1][i] == 1 and \
+                        grid[count + 1][i] == 0 or \
+                        grid[count - 1][i] == 0 and \
+                        grid[count + 1][i] == 1:
+                    perimeter += 1
+
+    return perimeter
